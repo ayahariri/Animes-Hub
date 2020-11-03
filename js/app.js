705f8617dbd6ae2b25871d53a2d2cc1f37ebb1c4
ayahariri <ayahariri899@hotmail.com>
2020-11-03 13:33:26 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'user strict'
 var anime = [
-    {name: "", category: "", rndmSeason:"",removeBtn:""}
+    { name: "", category: "", rndmSeason: "", removeBtn: "" }
 
 ];
 
@@ -9,24 +9,25 @@ function Anime(name, category, rndmSeason, removeBtn) {
     this.category = document.getElementById('animeCategory');
     this.rndmSeason = document.createElement('hd');
     this.removeBtn = document.createElement('hd');
-// Anime.prototype.length;
+    // Anime.prototype.length;
 }
 // console.log(Anime);
 Anime();
 
-function saveToLs(){
+function saveToLs() {
     var formResults = document.getElementById('formResult');
-localStorage.setItem("form", "form".value);
-// document.getElementById('formResult').innerHTML= localStorage.getItem()
+    localStorage.setItem("form", "form".value);
+    // document.getElementById('formResult').innerHTML= localStorage.getItem()
 }
 
 function getTable() {
     var table = document.getElementById('myTable').rows[0].cells.length;
     document.getElementById('result').innerHTML = ''
 }
-
+getTable();
 function getRndmSeason() {
     var season = Math.floor(Math.random() * 7);
     return Math.floor(Math.random() * 7);
 
 }
+getRndmSeason();
